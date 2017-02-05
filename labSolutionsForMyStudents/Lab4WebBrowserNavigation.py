6f3569eac9af3713765d499be948add18b4c2f44
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-02-05 15:06:33 -0700

    labSolutionsForMyStudents
    lab4 question1

---
+++ labSolutionsForMyStudents/Lab4WebBrowserNavigation.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 14:04:38 2017
+
+@author: zhangchi
+"""
+"""
+class Stack:
+    
+    def __init__(self):
+        self.__items = []
+        
+    def push(self, item):
+        self.__items.append(item)
+        
+    def pop(self):
+        return self.__items.pop()
+    
+    def peek(self):
+        return self.__items[len(self.__items)-1]
+    
+    def is_empty(self):
+        return len(self.__items) == 0
+    
+    def size(self):
+        return len(self.__items)
+"""       
+stack1 = []
+stack2 = []
+stack1.append("www.cs.ualberta.ca")
+print "current page: " + stack1[-1]
+while(True):
+    user_input = raw_input("Enter: ")
+    if user_input != "<" and user_input != ">":
+        if len(stack2)>0 :
+            stack2 = []
+        stack1.append(user_input)
+        print "current page: " + user_input
+    elif user_input == "<":    
+        if len(stack1)>1:
+            item = stack1.pop()
+            stack2.append(item)
+            print "current page: " + stack1[-1]
+        else:
+            print user_input + " is an invalid action"
+    elif user_input == ">":
+        try:
+            item = stack2.pop()
+            stack1.append(item)
+            print "current page: " + item
+        except:
+            print user_input + " is an invalid action"
+"""
+# version 1
+# pass eclass, but still exists problems
+stack1 = []
+stack2 = []
+last_user_input = None
+stack1.append("www.cs.ualberta.ca")
+print "current page: " + stack1[-1]
+while(True):
+    user_input = raw_input("Enter: ")
+    if user_input != "<" and user_input != ">":
+        if last_user_input == "<":
+            stack2 = []
+        stack1.append(user_input)
+        print "current page: " + user_input
+    elif user_input == "<":    
+        if len(stack1)>1:
+            item = stack1.pop()
+            stack2.append(item)
+            print "current page: " + stack1[-1]
+        else:
+            print user_input + " is an invalid action"
+    elif user_input == ">":
+        try:
+            item = stack2.pop()
+            stack1.append(item)
+            print "current page: " + item
+        except:
+            print user_input + " is an invalid action"
+    last_user_input = user_input
+"""
