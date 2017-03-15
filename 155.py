38cd7f8c87e7f29958b0b85efb114fcd470f9c26
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-15 10:53:53 -0600

    this is constant version, need review

--- 155.py
+++ 155.py
@@ -6,6 +6,59 @@ Created on Wed Mar 15 01:26:50 2017
 @author: zhangchi
 """
 
+#关键是要把每个时刻的最小值都给保存下来，那么就可以用list里套（）的方式
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        minValue = self.getMin()
+        if minValue is None or x < minValue:
+            minValue = x
+        self.stack.append((x,minValue))
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+                      
+'''
+
 class MinStack(object):
 
     def __init__(self):
@@ -20,10 +73,10 @@ class MinStack(object):
         """
         :type x: int
         :rtype: void
-        """
-        self.stack.append(x)
+        """        
         if self.minValue is None or x < self.minValue:
             self.minValue = x
+        self.stack.append((x,self.minValue))
         
 
     def pop(self):
@@ -31,26 +84,20 @@ class MinStack(object):
         :rtype: void
         """
         result = self.stack.pop()
-        if result == self.minValue:
-            self.minValue = None
-            for i in self.stack:
-                if self.minValue is None or i < self.minValue:
-                    self.minValue = i
-        return result
 
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.minValue
+        return self.stack[-1][1]
 
-
+'''
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(x)
