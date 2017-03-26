c1caae8ce8460f8f12a75b94b9fc156b6b6d5e81
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-25 21:23:35 -0600

    new version, need review

--- labSolutionsForMyStudents/hammingDistance.py
+++ labSolutionsForMyStudents/hammingDistance.py
@@ -12,6 +12,18 @@ class solution():
         self.result = []
         
     def hammingDistance(self,listNumber,count,stopPosition=[]):
+        if count == 0:
+            if listNumber not in self.result:
+                self.result.append(listNumber)
+        else:
+            count = count - 1
+            for i in range(len(listNumber)):
+                if i not in stopPosition:
+                    templistNumber = listNumber[:i] + [1^listNumber[i]] + listNumber[i+1:]
+                    tempStopPosition = copy.deepcopy(stopPosition)
+                    tempStopPosition.append(i)
+                    self.hammingDistance(templistNumber,count,tempStopPosition)
+        '''
         if count == 1:
             for i in range(len(listNumber)):
                 if i not in stopPosition:
@@ -26,12 +38,23 @@ class solution():
                     tempStopPosition = copy.deepcopy(stopPosition)
                     tempStopPosition.append(i)
                     self.hammingDistance(templistNumber,count,tempStopPosition)
+        '''
                     
 S = solution()
-listNumber = [0,0,0,0,0,1,1,1]
-count = 3
+listNumber = [0,0,0,0,0,1,1,1,0,0]
+count = 4
 S.hammingDistance(listNumber,count)
 print S.result
 print len(S.result)
-                        
+
+def humming(s,k):
+    if k==0:return [s]
+    else:
+        output=[]
+        for i in range(len(s)):
+            lists=humming(s[i+1:],k-1)
+            if lists is not None:
+                for item in lists:
+                    output.append(s[:i]+str(1^int(s[i]))+item)
+        return output
     
