a30693d1ad5c9454479cb0e43c46877b3b388b5e
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-02 01:18:51 -0600

    easy

---
+++ 441.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 01:10:28 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        step = 1
+        value = 
