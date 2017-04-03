852a67e7ae9446b9cf0b2fab623bfbf37c6292d6
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-03 14:52:38 -0600

    preorder, stack version, not use recursion

--- 144.py
+++ 144.py
@@ -7,11 +7,11 @@ Created on Thu Mar 30 23:16:59 2017
 """
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def preorderTraversal(self, root):
@@ -19,3 +19,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        if root is None:
+            return []
+        else:
+            temp = []
+            temp.append(root)
+            result = []
+            while len(temp) > 0:
+                point = temp.pop()
+                result.append(point.val)
+                if point.right is not None:
+                    temp.append(point.right)
+                if point.left is not None:
+                    temp.append(point.left)
+                
