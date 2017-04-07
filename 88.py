a00c98c9f7ec9c409f1576fe4b3a95891ddf2cf3
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-06 21:33:49 -0600

    inplace merge sort, review

---
+++ 88.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  6 20:59:01 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        a = 0
+        b = 0
+        count = 0
+        while a < m + count and b < n:
+            if nums1[a] < nums2[b]:
+                a += 1
+            else:
+                nums1[a+1:] = nums1[a:-1]
+                nums1[a] = nums2[b]
+                b += 1
+                a += 1
+                count += 1
+        for item in nums2[b:]:
+            nums1[a+1:] = nums1[a:-1]
+            nums1[a] = item
+            a += 1
+        return nums1
+    
+s = Solution()
+#print s.merge([1,2,4,8,0,0,0,0,0,0],4,[2,3,6,9],4)
+print s.merge([4,0,0,0,0,0],1,[2,3,6,9],4)
