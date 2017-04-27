98504f413dc9199554387ca5b121d20f27e4472c
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-26 19:04:20 -0600

    easy, update

--- 232.py
+++ 232.py
@@ -21,7 +21,13 @@ class MyQueue(object):
         :type x: int
         :rtype: void
         """
-        self.stack = [x] + self.stack
+        #self.stack = [x] + self.stack
+        temp = []
+        while len(self.stack) > 0:
+            temp.append(self.stack.pop())
+        self.stack.append(x)
+        while len(temp) > 0:
+            self.stack.append(temp.pop())
 
     def pop(self):
         """
