ea43425a5662c2e9ef90d0ba8e9837736a1fc68c
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-27 08:12:08 -0700

    review, second version of Queen N

--- 51.py
+++ 51.py
@@ -27,8 +27,11 @@ class Solution(object):
             self.result.append(alist)
         else:
             for i in range(n):
-                if i not in alist:
-                    self.helper(n, alist+[i])
+                if self.checkValid(alist):
+                    if i not in alist:
+                        self.helper(n, alist+[i])
+                else:
+                    break
             
     def checkValid(self, alist):
         temp = []
@@ -50,6 +53,53 @@ class Solution(object):
         for item in alist:
             temp.append('.'*item+'Q'+'.'*(n-item-1))
         return temp
+        
+#==============================================================================
+# class Solution(object):
+#     def __init__(self):
+#         self.result = []
+# 
+#     def solveNQueens(self, n):
+#         """
+#         :type n: int
+#         :rtype: List[List[str]]
+#         """
+#         self.helper(n, [])
+#         finalResult = []
+#         for item in self.result:
+#             if self.checkValid(item) == True:
+#                 finalResult.append(self.changeStyle(item,n))
+#         return finalResult
+#     
+#     def helper(self, n, alist):
+#         if len(alist) == n:
+#             self.result.append(alist)
+#         else:
+#             for i in range(n):
+#                 if i not in alist:
+#                     self.helper(n, alist+[i])
+#             
+#     def checkValid(self, alist):
+#         temp = []
+#         for i, j in enumerate(alist):
+#             if (i+j) not in temp:
+#                 temp.append(i+j)
+#             else:
+#                 return False
+#         temp = []
+#         for i, j in enumerate(alist):
+#             if (i-j) not in temp:
+#                 temp.append(i-j)
+#             else:
+#                 return False
+#         return True
+#         
+#     def changeStyle(self, alist, n):
+#         temp = []
+#         for item in alist:
+#             temp.append('.'*item+'Q'+'.'*(n-item-1))
+#         return temp
+#==============================================================================
                     
 s = Solution()
-print s.solveNQueens(8)
+print s.solveNQueens(9)
