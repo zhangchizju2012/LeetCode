d7291e5e34856223387b6ea4fdcd3cc53b66f9d7
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-26 19:11:12 -0600

    review, solution is really clever

--- 232.py
+++ 232.py
@@ -53,6 +53,50 @@ class MyQueue(object):
         return len(self.stack) == 0
 
 
+#==============================================================================
+# class Queue(object):
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.inStack, self.outStack = [], []
+# 
+#     def push(self, x):
+#         """
+#         :type x: int
+#         :rtype: nothing
+#         """
+#         self.inStack.append(x)
+# 
+#     def pop(self):
+#         """
+#         :rtype: nothing
+#         """
+#         self.move()
+#         self.outStack.pop()
+# 
+#     def peek(self):
+#         """
+#         :rtype: int
+#         """
+#         self.move()
+#         return self.outStack[-1]
+# 
+#     def empty(self):
+#         """
+#         :rtype: bool
+#         """
+#         return (not self.inStack) and (not self.outStack) 
+#         
+#     def move(self):
+#         """
+#         :rtype nothing
+#         """
+#         if not self.outStack:
+#             while self.inStack:
+#                 self.outStack.append(self.inStack.pop())
+#==============================================================================
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
