f749c5a21ec2fc42b0f5ec513edb4fad01d39a29
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-30 06:16:26 +0800

    review, number of islands, need improved

---
+++ 200.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 17:13:47 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        islandList = []
+        row = len(grid)
+        if row == 0:
+            return 0
+        col = len(grid[0])
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '1':
+                    islandList.append((i,j))
+        
+        result = 0
+        tempList = []
+        while len(islandList) > 0:
+            result += 1
+            tempList.append(islandList.pop())
+            while len(tempList) > 0:
+                temp = tempList.pop()
+                try:
+                    if (temp[0]-1,temp[1]) in islandList:
+                        tempList.append((temp[0]-1,temp[1]))
+                        islandList.remove((temp[0]-1,temp[1]))
+                except:
+                    pass
+                try:
+                    if (temp[0]+1,temp[1]) in islandList:
+                        tempList.append((temp[0]+1,temp[1]))
+                        islandList.remove((temp[0]+1,temp[1]))
+                except:
+                    pass
+                try:
+                    if (temp[0],temp[1]-1) in islandList:
+                        tempList.append((temp[0],temp[1]-1))
+                        islandList.remove((temp[0],temp[1]-1))
+                except:
+                    pass
+                try:
+                    if (temp[0],temp[1]+1) in islandList:
+                        tempList.append((temp[0],temp[1]+1))
+                        islandList.remove((temp[0],temp[1]+1))
+                except:
+                    pass
+        return result
+        
+s = Solution()
+print s.numIslands(["11110","11010","11000","00000"])
+
+#==============================================================================
+# class Solution(object):
+#     def numIslands(self, grid):
+#         """
+#         :type grid: List[List[str]]
+#         :rtype: int
+#         """
+#         islandList = []
+#         row = len(grid)
+#         col = len(grid[0])
+#         for i in range(row):
+#             for j in range(col):
+#                 if grid[i][j] == '1':
+#                     islandList.append((i,j))
+#         
+#         result = 0
+#         tempList = []
+#         while len(islandList) > 0:
+#             result += 1
+#             tempList.append(islandList.pop())
+#             while len(tempList) > 0:
+#                 temp = tempList.pop()
+#                 try:
+#                     if grid[temp[0]-1][temp[1]] == '1':
+#                         tempList.append((temp[0]-1,temp[1]))
+#                         islandList.remove((temp[0]-1,temp[1]))
+#                 except:
+#                     pass
+#                 try:
+#                     if grid[temp[0]+1][temp[1]] == '1':
+#                         tempList.append((temp[0]+1,temp[1]))
+#                         islandList.remove((temp[0]+1,temp[1]))
+#                 except:
+#                     pass
+#                 try:
+#                     if grid[temp[0]][temp[1]-1] == '1':
+#                         tempList.append((temp[0],temp[1]-1))
+#                         islandList.remove((temp[0],temp[1]-1))
+#                 except:
+#                     pass
+#                 try:
+#                     if grid[temp[0]][temp[1]+1] == '1':
+#                         tempList.append((temp[0],temp[1]+1))
+#                         islandList.remove((temp[0],temp[1]+1))
+#                 except:
+#                     pass
+#         return result
+#==============================================================================
