6512709a4c4521dfe689857b655746697233c0c8
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-30 06:42:20 +0800

    review, bfs, should can be also implemented by dfs

--- 200.py
+++ 200.py
@@ -12,7 +12,7 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        islandList = []
+        islandList = {}
         row = len(grid)
         if row == 0:
             return 0
@@ -20,37 +20,37 @@ class Solution(object):
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == '1':
-                    islandList.append((i,j))
+                    islandList[(i,j)] = 1
         
         result = 0
         tempList = []
         while len(islandList) > 0:
             result += 1
-            tempList.append(islandList.pop())
+            tempList.append(islandList.popitem()[0])
             while len(tempList) > 0:
                 temp = tempList.pop()
                 try:
                     if (temp[0]-1,temp[1]) in islandList:
                         tempList.append((temp[0]-1,temp[1]))
-                        islandList.remove((temp[0]-1,temp[1]))
+                        islandList.pop((temp[0]-1,temp[1]))
                 except:
                     pass
                 try:
                     if (temp[0]+1,temp[1]) in islandList:
                         tempList.append((temp[0]+1,temp[1]))
-                        islandList.remove((temp[0]+1,temp[1]))
+                        islandList.pop((temp[0]+1,temp[1]))
                 except:
                     pass
                 try:
                     if (temp[0],temp[1]-1) in islandList:
                         tempList.append((temp[0],temp[1]-1))
-                        islandList.remove((temp[0],temp[1]-1))
+                        islandList.pop((temp[0],temp[1]-1))
                 except:
                     pass
                 try:
                     if (temp[0],temp[1]+1) in islandList:
                         tempList.append((temp[0],temp[1]+1))
-                        islandList.remove((temp[0],temp[1]+1))
+                        islandList.pop((temp[0],temp[1]+1))
                 except:
                     pass
         return result
@@ -59,6 +59,60 @@ s = Solution()
 print s.numIslands(["11110","11010","11000","00000"])
 
 #==============================================================================
+# can be improved by using hash table
+# class Solution(object):
+#     def numIslands(self, grid):
+#         """
+#         :type grid: List[List[str]]
+#         :rtype: int
+#         """
+#         islandList = []
+#         row = len(grid)
+#         if row == 0:
+#             return 0
+#         col = len(grid[0])
+#         for i in range(row):
+#             for j in range(col):
+#                 if grid[i][j] == '1':
+#                     islandList.append((i,j))
+#         
+#         result = 0
+#         tempList = []
+#         while len(islandList) > 0:
+#             result += 1
+#             tempList.append(islandList.pop())
+#             while len(tempList) > 0:
+#                 temp = tempList.pop()
+#                 try:
+#                     if (temp[0]-1,temp[1]) in islandList:
+#                         tempList.append((temp[0]-1,temp[1]))
+#                         islandList.remove((temp[0]-1,temp[1]))
+#                 except:
+#                     pass
+#                 try:
+#                     if (temp[0]+1,temp[1]) in islandList:
+#                         tempList.append((temp[0]+1,temp[1]))
+#                         islandList.remove((temp[0]+1,temp[1]))
+#                 except:
+#                     pass
+#                 try:
+#                     if (temp[0],temp[1]-1) in islandList:
+#                         tempList.append((temp[0],temp[1]-1))
+#                         islandList.remove((temp[0],temp[1]-1))
+#                 except:
+#                     pass
+#                 try:
+#                     if (temp[0],temp[1]+1) in islandList:
+#                         tempList.append((temp[0],temp[1]+1))
+#                         islandList.remove((temp[0],temp[1]+1))
+#                 except:
+#                     pass
+#         return result
+# 
+#==============================================================================
+
+#==============================================================================
+# can be improved by 上一个方法
 # class Solution(object):
 #     def numIslands(self, grid):
 #         """
