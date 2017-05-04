7b87213d7b1155c01d20e81245d6ea47cef9c85b
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-04 22:36:53 +0800

    review, given a list and a target, check whether its subset can sum to the target

---
+++ 416.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 20:49:46 2017
+
+@author: zhangchi
+"""
+
+# 关键就是给定一串数，是否有子集的和能与给定值相同
+# 最开始时候我自己的想法是产生所有的子集，然后与target对比
+
+class Solution(object):
+    # inspired by https://discuss.leetcode.com/topic/67539/0-1-knapsack-detailed-explanation
+    # but improved this solution a lot.
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        target = total / 2
+        previous = {0:1} # improved by adding hashmap compared with the sencond.
+        for item in nums:
+            temp = {}
+            for number in previous:
+                if number + item == target:
+                    return True
+                elif number + item not in previous: # 可以进一步优化，当number + item > target，就可以扔掉了
+                    temp[number + item] = 1
+            previous.update(temp) # combine two dictionary
+        return False
+
+#==============================================================================
+# class Solution(object):
+#     def canPartition(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         total = sum(nums)
+#         if total % 2 == 1:
+#             return False
+#         target = total / 2
+#         previous = [0]
+#         for item in nums:
+#             temp = []
+#             for number in previous:
+#                 if number + item == target:
+#                     return True
+#                 elif number + item not in previous: # improved by checking "in previous" compared with the third.
+#                     temp.append(number + item)
+#             previous = previous + temp
+#         return False
+#==============================================================================
+
+#==============================================================================
+# class Solution(object):
+#     # slow, brute force
+#     def canPartition(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         total = sum(nums)
+#         if total % 2 == 1:
+#             return False
+#         target = total / 2
+#         previous = [0]
+#         for item in nums:
+#             temp = []
+#             for number in previous:
+#                 if number + item == target:
+#                     return True
+#                 else:
+#                     temp.append(number + item)
+#             previous = previous + temp
+#         return False
+#==============================================================================
+
+s = Solution()
+print s.canPartition([100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100])
