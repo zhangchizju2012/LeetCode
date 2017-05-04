44572d6a44014852d42d0b29f15eb231111c8349
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-04 22:52:42 +0800

    review, given a list and a target, check whether its subset can sum to the target, new way to generate subset

--- 416.py
+++ 416.py
@@ -7,7 +7,19 @@ Created on Thu May  4 20:49:46 2017
 """
 
 # 关键就是给定一串数，是否有子集的和能与给定值相同
-# 最开始时候我自己的想法是产生所有的子集，然后与target对比
+# 最开始时候我自己的想法是产生所有的子集(用dfs,或这bit manipulation的方法)，然后与target对比
+
+#==============================================================================
+# 由这题产生了新的产生子集的思路
+# nums=[1,2,3,4]
+# previous = [[]]
+# for item in nums:
+#     temp = []
+#     for number in previous:
+#         temp.append(number + [item])
+#     previous = previous + temp
+# 最后所有子集就在previous里
+#==============================================================================
 
 class Solution(object):
     # inspired by https://discuss.leetcode.com/topic/67539/0-1-knapsack-detailed-explanation
