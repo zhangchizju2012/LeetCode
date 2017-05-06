6cb6129d3e11fd11c6dd990ad3149114904b52a3
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-06 09:47:33 +0800

    review 55, easy after finish 55

--- 45.py
+++ 45.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 12 01:20:40 2017
+Created on Sat May  6 09:38:51 2017
 
 @author: zhangchi
 """
@@ -12,3 +12,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        length = len(nums) - 1
+        if nums[0] >= length:
+            return 1
+        previous = 0
+        now = nums[0]
+        temp = - float('inf')
+        count = 1
+        while True:
+            count += 1
+            for i in xrange(previous+1,now+1):
+                temp = max(temp, i+nums[i])
+                if temp >= length:
+                    return count
+            previous = now
+            now = temp
