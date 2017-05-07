2f6f831e2cff3b592dd3cb746ac1ab7bec5be206
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-07 09:47:26 +0800

    contest 5, easy

---
+++ 575.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 09:27:43 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for item in candies:
+            if item in dic:
+                dic[item] += 1
+            else:
+                dic[item] = 1
+        total = len(candies)/2
+        if len(dic) >= total:
+            return total
+        else:
+            return len(dic)
+        
+
+
+s = Solution()
+print s.distributeCandies([1,1,2,3])
