0d5fe99700e9fdd08277338029cf9c17d4182032
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-13 23:48:06 +0800

    dp, bfs, dfs

--- 322.py
+++ 322.py
@@ -7,6 +7,7 @@ Created on Sat May 13 22:13:24 2017
 """
 
 class Solution(object):
+    # bfs
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
@@ -15,28 +16,62 @@ class Solution(object):
         """
         if amount == 0:
             return 0
-        result = {}
-        for item in coins:
-            result[item] = 1
-        for i in xrange(1,amount+1):
-            if i not in result:
-                minValue = float('inf')
-                label = False
-                for j in xrange(1,i//2+1):
-                    if j in result and (i-j) in result:
-                        minValue = min(minValue,result[j]+result[i-j])
-                        label = True
-                if label == True:
-                    result[i] = minValue
-        if amount in result:
-            return result[amount]
-        else:
-            return -1
+        stack = [amount]
+        searched = [amount]
+        result = 0
+        while len(stack) > 0:
+            result += 1
+            temp = []
+            while len(stack) > 0:
+                point = stack.pop()
+                for i in coins:
+                    value = point - i
+                    if value == 0:
+                        return result
+                    elif value > 0 and value not in searched:
+                        temp.append(value)
+                        searched.append(value)
+            stack = temp
+        return -1
+                    
+s = Solution()
+#print s.coinChange([3,7,405,436],8839)
+print s.coinChange([2],3)
+
+#==============================================================================
+# class Solution(object):
+#     # dp
+#     def coinChange(self, coins, amount):
+#         """
+#         :type coins: List[int]
+#         :type amount: int
+#         :rtype: int
+#         """
+#         if amount == 0:
+#             return 0
+#         result = {}
+#         for item in coins:
+#             result[item] = 1
+#         for i in xrange(1,amount+1):
+#             if i not in result:
+#                 minValue = float('inf')
+#                 label = False
+#                 for j in xrange(1,i//2+1):
+#                     if j in result and (i-j) in result:
+#                         minValue = min(minValue,result[j]+result[i-j])
+#                         label = True
+#                 if label == True:
+#                     result[i] = minValue
+#         if amount in result:
+#             return result[amount]
+#         else:
+#             return -1
+#==============================================================================
                 
 
 #==============================================================================
 # class Solution(object):
-#     # too slow
+#     # too slow, dfs
 #     def coinChange(self, coins, amount):
 #         """
 #         :type coins: List[int]
@@ -59,5 +94,3 @@ class Solution(object):
 #                 self.helper(amount-item,count+1)
 #==============================================================================
                 
-s = Solution()
-print s.coinChange([1,2,5],100)
