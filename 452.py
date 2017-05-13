e6019c81bfde7ee0c43a1af40910b066146ffbb8
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-13 18:02:42 +0800

    greedy, notes about sort

---
+++ 452.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 17:41:46 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort(key=lambda x:x[1])
+        end = -float('inf')
+        result = 0
+        for a,b in points:
+            if a > end:
+                result += 1
+                end = b
+        return result
