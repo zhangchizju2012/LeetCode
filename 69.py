8912f5feecdbcb5ee08504063b47c83586aef09b
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-16 18:08:03 +0800

    improved by binary search, 2 versions

--- 69.py
+++ 69.py
@@ -14,16 +14,43 @@ class Solution(object):
         """
         if x <= 0:
             return x
-        count = 0
+        left = 1
+        right = x
+        if left ** 2 <= x and (left + 1) ** 2 > x:
+            return left
+        if right ** 2 <= x and (right + 1) ** 2 > x:
+            return right
         while True:
-            if (2 ** count) ** 2 > x:
-                break
-            count += 1
-        previous = 2 ** (count-1)
-        for item in xrange(2**(count-1),2**count+1):
-            if item ** 2 > x:
-                return previous
-            previous = item
-            
+            middle = (left + right) // 2
+            if middle ** 2 <= x and (middle + 1) ** 2 > x:
+                return middle
+            elif middle ** 2 <= x and (middle + 1) ** 2 <= x:
+                left = middle
+            elif middle ** 2 > x and (middle + 1) ** 2 > x:
+                right = middle
+
+#==============================================================================
+# class Solution(object):
+#     # almost brute force
+#     def mySqrt(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+#         if x <= 0:
+#             return x
+#         count = 0
+#         while True:
+#             if (2 ** count) ** 2 > x:
+#                 break
+#             count += 1
+#         # 通过2的阶乘来缩小范围
+#         previous = 2 ** (count-1)
+#         for item in xrange(2**(count-1),2**count+1):
+#             if item ** 2 > x:
+#                 return previous
+#             previous = item
+#             
+#==============================================================================
 s = Solution()
-print s.mySqrt(16)
+print s.mySqrt(9)
