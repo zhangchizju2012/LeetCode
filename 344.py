0583ab3bd3b77e8bd3321e1a12e1a11d6a235ac9
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-05-18 10:15:52 +0800

    stupid easy

---
+++ 344.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 10:15:22 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
