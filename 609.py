5100b721217b3c0a1d2b1200f0b07074adb4fb35
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-03 20:38:28 -0600

    easy

--- 609.py
+++ 609.py
@@ -12,7 +12,23 @@ class Solution(object):
         :type paths: List[str]
         :rtype: List[List[str]]
         """
-        
+        dic = {}
+        for item in paths:
+            temp = item.split(" ")
+            if len(temp) > 1:
+                for i in xrange(1,len(temp)):
+                    positionA = temp[i].find("(")
+                    positionB = temp[i].find(")")
+                    txt = temp[i][positionA+1:positionB]
+                    if txt in dic:
+                        dic[txt].append(temp[0]+'/'+temp[i][:positionA])
+                    else:
+                        dic[txt] = [temp[0]+'/'+temp[i][:positionA]]
+        result = []
+        for item in dic:
+            if len(dic[item]) > 1:
+                result.append(dic[item])
+        return result
         
 s = Solution()
 print s.findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"])
