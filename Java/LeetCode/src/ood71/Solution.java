bd21ce0a2757c4383b1c9d579cb0e8763355219b
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-07 22:47:01 -0600

    first study of OOD, understand enum, understand how to new object based on public class Hand <T extends Card>

---
+++ Java/LeetCode/src/ood71/Solution.java
@@ -0,0 +1,20 @@
+package ood71;
+
+public class Solution {
+	public static void main(String[] args){
+		// 学会如何根据public class Hand <T extends Card> 来new出新的对象
+		Hand<Card> temp = new Hand<>();
+		// 学会如何使用enum
+		Suit suit = Suit.Club;
+		Card card = new Card(3,suit) {
+			
+			@Override
+			public int value() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		};
+		temp.addCard(card);
+		temp.print();
+	}
+}
