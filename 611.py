ac464f33280be80a0ee5d79751e2bb12e16b88da
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-12 18:31:08 -0600

    review, hard, contest 10, two pointers, add notes

--- 611.py
+++ 611.py
@@ -18,22 +18,31 @@ class Solution(object):
             return 0
         tempResult = []
         for i in xrange(length-2):
+            # 把第一数固定住，然后后两个数由指针挪动来决定
             first = nums[i]
+            # 两个指针挪过去
             pointA = i + 1
             pointB = i + 2
             count = 0
             while pointB < length:
                 if first + nums[pointA] > nums[pointB]:
+                    # 这个比较好理解，如果前两个数相加足够大，可以让后一个数再大一些
                     count += 1
                     pointB += 1
                 else:
                     tempResult.append(count)
                     pointA += 1
                     count -= 1
+                    # 为了减少循环次数，这里只改变pointA, pointB没变化，count -= 1的目的是，如果挪动
+                    # pointA之后满足条件了的话，会在line30那里再把1加上
                     if pointA == pointB:
+                    # 如果pointA == pointB, 有点像是从line24那里重新开始，pointA,pointB之间的距离还是1，
+                    # 但是他们相对于line24已经同时往右平移了
                         pointB += 1
                         count = 0
             tempResult.append((count+1)*count/2)
+            # pointB顶到最右端了，需要把n,n-1,n-2,...1都加上（因为pointA还没有到最后第二那里）
+            # 但是pointB能到最右端说明此时pointA是可以任意移动的
         return tempResult, sum(tempResult)
         
 s = Solution()
