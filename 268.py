7211b17ae816d75eb540772a532d02145e62e2b6
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-21 17:42:41 -0600

    review, 3 version, missing number

--- 268.py
+++ 268.py
@@ -7,11 +7,21 @@ Created on Wed Jun 21 13:08:53 2017
 """
 
 class Solution(object):
+    # 还有位控制的一种方法，来自答案，值得一看
+    # https://discuss.leetcode.com/topic/24535/4-line-simple-java-bit-manipulate-solution-with-explaination
     def missingNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        # 因为之差一个数，如果全都在的话是0,1,2,3...len(nums)
+        return len(nums) * (len(nums) + 1) / 2 - sum(nums)
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 利用hash，很二
         dic = set()
         for item in nums:
             dic.add(item)
