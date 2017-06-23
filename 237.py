644d9f506fc83bc3232bc6dbd673fdaaa98b77fa
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-23 00:55:36 -0600

    review, 题有问题，其实不是真的delete node,所以一开始想不到

---
+++ 237.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 00:41:05 2017
+
+@author: zhangchi
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
