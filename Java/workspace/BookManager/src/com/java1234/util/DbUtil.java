28550fb5c5e68248861c3bd7c07f6b0ce8a6fa4d
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-06-28 12:42:13 -0600

    first study of jdbc

---
+++ Java/workspace/BookManager/src/com/java1234/util/DbUtil.java
@@ -0,0 +1,39 @@
+package com.java1234.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ * 数据库工具类
+ * @author ZHANG CHI
+ *
+ */
+
+public class DbUtil {
+	private String dbUrl="jdbc:mysql://localhost:3306/db_book";
+	private String dbUserName="root";
+	private String dbPassword="105009";
+	private String jdbcName="com.mysql.jdbc.Driver";
+	
+	public Connection getCon() throws Exception{
+		Class.forName(jdbcName);
+		Connection con=DriverManager.getConnection(dbUrl,dbUserName,dbPassword);
+		return con;
+	}
+	
+	public void closeCon(Connection con) throws Exception{
+		if(con!=null){
+			con.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DbUtil dbUtil=new DbUtil();
+		try {
+			dbUtil.getCon();
+			System.out.println("成功");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Java/workspace/BookManager/src/com/java1234/view/LogOnFrm.java
@@ -0,0 +1,182 @@
+package com.java1234.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.java1234.dao.UserDao;
+import com.java1234.model.User;
+import com.java1234.util.DbUtil;
+import com.java1234.util.StringUtil;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.awt.event.ActionEvent;
+
+public class LogOnFrm extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField userNameTxt;
+	private JPasswordField passwordTxt;
+	
+	private DbUtil dbUtil = new DbUtil();
+	private UserDao userDao = new UserDao();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LogOnFrm frame = new LogOnFrm();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LogOnFrm() {
+		setResizable(false);
+		setTitle("管理员登录");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 391);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel label = new JLabel("图书管理系统");
+		label.setFont(new Font("Lucida Grande", Font.BOLD, 23));
+		label.setIcon(new ImageIcon(LogOnFrm.class.getResource("/images/logo.png")));
+		
+		JLabel lblNewLabel = new JLabel("用户名：");
+		lblNewLabel.setIcon(new ImageIcon(LogOnFrm.class.getResource("/images/userName.png")));
+		
+		JLabel lblNewLabel_1 = new JLabel("密码：");
+		lblNewLabel_1.setIcon(new ImageIcon(LogOnFrm.class.getResource("/images/password.png")));
+		
+		userNameTxt = new JTextField();
+		userNameTxt.setColumns(10);
+		
+		passwordTxt = new JPasswordField();
+		
+		JButton btnNewButton = new JButton("登录");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loginActionPerformed(e);
+			}
+		});
+		btnNewButton.setIcon(new ImageIcon(LogOnFrm.class.getResource("/images/login.png")));
+		
+		JButton btnNewButton_1 = new JButton("重置");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resetValueActionPerformed(e);
+			}
+		});
+		btnNewButton_1.setIcon(new ImageIcon(LogOnFrm.class.getResource("/images/reset.png")));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(133)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(label)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblNewLabel)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(userNameTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+									.addComponent(btnNewButton_1)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(lblNewLabel_1)
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(passwordTxt, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(143)
+							.addComponent(btnNewButton)))
+					.addContainerGap(99, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(54)
+					.addComponent(label)
+					.addGap(31)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(userNameTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(28)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(passwordTxt, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(btnNewButton_1))
+					.addContainerGap(80, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	/**
+	 * 登录事件处理
+	 * @param e
+	 */
+	private void loginActionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String userName = this.userNameTxt.getText();
+		String password = new String(this.passwordTxt.getPassword());
+		if(StringUtil.isEmpty(userName)){
+			JOptionPane.showMessageDialog(null, "用户名不能为空！");
+			return;
+		}
+		if(StringUtil.isEmpty(password)){
+			JOptionPane.showMessageDialog(null, "密码不能为空！");
+			return;
+		}
+		User user = new User(userName,password);
+		Connection con = null;
+		try {
+			con = dbUtil.getCon();
+			User currentUser = userDao.login(con, user);
+			if(currentUser!=null){
+				JOptionPane.showMessageDialog(null, "登录成功！");
+			}else{
+				JOptionPane.showMessageDialog(null, "用户名或者密码错误！");
+			}
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * 重置时间处理
+	 * @param evt
+	 */
+
+	private void resetValueActionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		this.userNameTxt.setText("");
+		this.passwordTxt.setText("");
+	}
+}
