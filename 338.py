48f331ecb5f6479452a55af31b2f77532a86d3ac
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-07-08 11:44:56 -0600

    review, optimized version

--- 338.py
+++ 338.py
@@ -5,26 +5,51 @@ Created on Fri Jul  7 23:32:58 2017
 
 @author: zhangchi
 """
-
 class Solution(object):
+    # O(n) 复杂度，成对出现，第一次是2，第二次是4，再是8，每次结果加1
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
-        result = []
-        for i in xrange(num+1):
-            result.append(self.helper(i))
-        return result
-        
-    def helper(self, n):
-        count = 0
-        temp = 1
-        while temp <= n:
-            if temp & n > 0:
+        if num == 0:
+            return [0]
+        if num == 1:
+            return [0,1]
+        result = [0,1]
+        count = 1
+        nextResult = []
+        while True:
+            for item in result:
+                nextResult.append(item+1)
                 count += 1
-            temp *= 2
-        return count
+                if count == num:
+                    return result + nextResult
+            result += nextResult
+            nextResult = []
+
+# 下面的也可以，复杂度较高
+#==============================================================================
+# class Solution(object):
+#     def countBits(self, num):
+#         """
+#         :type num: int
+#         :rtype: List[int]
+#         """
+#         result = []
+#         for i in xrange(num+1):
+#             result.append(self.helper(i))
+#         return result
+#         
+#     def helper(self, n):
+#         count = 0
+#         temp = 1
+#         while temp <= n:
+#             if temp & n > 0:
+#                 count += 1
+#             temp *= 2
+#         return count
+#==============================================================================
         
 s = Solution()
-print s.countBits(5)
+print s.countBits(4)
