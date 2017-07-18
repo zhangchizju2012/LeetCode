7668112772e61a7d3bfd34afa6634ed0ee07f51a
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-07-18 01:15:25 -0600

    easy

---
+++ 73.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 01:07:59 2017
+
+@author: zhangchi
+"""
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+        dicR = set()
+        dicC = set()
+        for i in xrange(row):
+            for j in xrange(col):
+                if matrix[i][j] == 0:
+                    dicR.add(i)
+                    dicC.add(j)
+        for item in dicR:
+            for i in xrange(col):
+                matrix[item][i] = 0
+        for item in dicC:
+            for i in xrange(row):
+                matrix[i][item] = 0
