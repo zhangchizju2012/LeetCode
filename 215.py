34f36c1ae1c4a3d881e68b324a3bf766a9f786aa
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-08-03 23:18:05 -0600

    quick sort, review

--- 215.py
+++ 215.py
@@ -1,36 +1,68 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 13 22:38:11 2017
+Review on Thu Aug  3 22:59:21 2017
 
 @author: zhangchi
 """
 
-import heapq
-
 class Solution(object):
-    # complexity comparison can be found here:
-    # https://leetcode.com/problems/kth-largest-element-in-an-array/#/solutions
     def findKthLargest(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        heap = []
-        for item in nums:
-            heapq.heappush(heap,-item)
-        count = 1
-        while count <= k:
-            result = -heapq.heappop(heap)
-            count += 1
-        return result
+        self.nums = nums
         
-    def findKthLargest2(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        nums.sort(reverse=True)
-        return nums[k-1]
+    def partition(self, first, last):
+        pivot = self.nums[first]
+        left = first + 1
+        right = last
+        while True:
+            while left <= right and self.nums[left] >= pivot:
+                left += 1
+            while right >= left and self.nums[right] <= pivot:
+                right -= 1
+            if left < right:
+                
+            
+        
+
+
+"""
+Created on Thu Apr 13 22:38:11 2017
+
+@author: zhangchi
+"""
+
+#==============================================================================
+# import heapq
+# 
+# class Solution(object):
+#     # complexity comparison can be found here:
+#     # https://leetcode.com/problems/kth-largest-element-in-an-array/#/solutions
+#     def findKthLargest(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
+#         heap = []
+#         for item in nums:
+#             heapq.heappush(heap,-item)
+#         count = 1
+#         while count <= k:
+#             result = -heapq.heappop(heap)
+#             count += 1
+#         return result
+#         
+#     def findKthLargest2(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
+#         nums.sort(reverse=True)
+#         return nums[k-1]
+#==============================================================================
