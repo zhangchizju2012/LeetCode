1c3606a3233609cba456e9190c373af1441d2ec5
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-09-08 23:08:32 -0600

    hard

---
+++ 146.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 23:06:01 2017
+
+@author: zhangchi
+"""
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.pre = None
+        self.next = None
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        
