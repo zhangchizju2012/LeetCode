000ab896f2f46da3fe6c27a1ee364c745c9433c4
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-09-14 18:03:27 -0600

    review, first round review 再复习

--- 131.py
+++ 131.py
@@ -6,6 +6,15 @@ Created on Sat May  6 21:15:49 2017
 @author: zhangchi
 """
 
+# 0914.2017复习，
+# 另一种想法，从左到右，从短到常考虑
+# 可以从左到右扫描，先考虑一个字符，一个字符直接就是回文了，然后增加一个，然后两个分开肯定是回文，然后再往前扫描
+# 还可能存在回文的。
+# 当前面存在n个了的时候，再加一个，然后这一个加上前m（需要循环过去），可能还是回文，然后和前m之前的（通过dic查询），进行合并
+# 这一小部分有点像我uncertain graph link prediction的算法
+# dic里储存，长度为m时的结果
+
+
 class Solution(object):
     # DP, use for loop instead of recursion
     def partition(self, s):
@@ -30,4 +39,7 @@ class Solution(object):
                         dic[i].append([temp]+item)
         return dic[0]
                 
+s = Solution()
+a = "aab"
+print s.partition(a)
         
