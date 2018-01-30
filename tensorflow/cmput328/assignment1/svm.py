90d6b05dce3f027b4adb7ca60b0415df3dcb280e
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2018-01-29 22:29:27 -0700

    fix one_hot bug

--- tensorflow/cmput328/assignment1/svm.py
+++ tensorflow/cmput328/assignment1/svm.py
@@ -14,39 +14,40 @@ import timeit
 
 def run():#x_test):
     n_samples = 55000
-    batch_size = 10
+    batch_size = 5
     
     # parameters settings
-    mnist = read_data_sets("data", one_hot=False)
+    mnist = read_data_sets("data", one_hot=True)
     x_train, y_train = mnist.train.images, mnist.train.labels
+    shape = y_train.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if y_train[i,j] == 0.:
+                y_train[i,j] = -1.
     
     # Define placeholders for input
     X = tf.placeholder(tf.float32, shape=(batch_size, 784))
-    y = tf.placeholder(tf.float32, shape=(batch_size, 1))
+    y = tf.placeholder(tf.float32, shape=(batch_size, 10))
     
     with tf.variable_scope("support-vector-machine"):
-        W = tf.get_variable("weights", (784, 1),
+        W = tf.get_variable("weights", (784, 10),
                             initializer=tf.random_normal_initializer())
-        b = tf.get_variable("bias", (1,),
-                            initializer=tf.constant_initializer(0.0))
+        b = tf.get_variable("bias", (10,),
+                            initializer=tf.random_normal_initializer())
+                            #initializer=tf.constant_initializer(0.0))
         y_pred = tf.matmul(X, W) + b
         temp = tf.maximum(0., 1 - tf.multiply(y, y_pred))
-        #loss = tf.reduce_sum((y - y_pred)**2)
+        loss = tf.reduce_sum(temp, axis = 0)
         
     with tf.Session() as sess:
         # Initialize Variables in graph
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         indices = np.random.choice(n_samples, batch_size)
         X_batch, y_batch = x_train[indices], y_train[indices]
-        y_batch = np.reshape(y_batch, (batch_size,1))
-        y_pred_, y_, temp_ = sess.run([y_pred, y, temp], feed_dict={X: X_batch, y: y_batch})
-        print(y_batch)
-        print(y_pred_)
-        print(y_)
+        #y_batch = np.reshape(y_batch, (batch_size,1))
+        temp_, loss_ = sess.run([temp, loss], feed_dict={X: X_batch, y: y_batch})
         print(temp_)
-        #_, loss_val, W_, b_ = sess.run([opt_operation, loss, W, b], feed_dict={X: X_batch, y: y_batch})
-        #print loss_val
-        #print(str(loss_val)+ " "+ str(W_)+" "+str(b_))
+        print(loss_)
     
     predicted_y_test = []
     
