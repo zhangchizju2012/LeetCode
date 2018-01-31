04333b7e26eee8d5985964db1e1524cb4cf0d11e
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2018-01-30 21:58:36 -0700

    done

--- tensorflow/cmput328/assignment1/svm.py
+++ tensorflow/cmput328/assignment1/svm.py
@@ -12,12 +12,12 @@ from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 import timeit
 
 
-def run():#x_test):
+def run(x_test):
     n_samples = 55000
     batch_size = 128 # 5
     lr = 0.05
     C = 0.002
-    num_epochs=50
+    num_epochs = 50
     
     # parameters settings
     mnist = read_data_sets("data", one_hot=True)
@@ -29,8 +29,10 @@ def run():#x_test):
                 y_train[i,j] = -1.
     
     # Define placeholders for input
-    X = tf.placeholder(tf.float32, shape=(batch_size, 784))
-    y = tf.placeholder(tf.float32, shape=(batch_size, 10))
+    # Pay attention to None here, I use batch_size originally
+    # None can handle any size, which is better.
+    X = tf.placeholder(tf.float32, shape=(None, 784))
+    y = tf.placeholder(tf.float32, shape=(None, 10))
     
     with tf.variable_scope("support-vector-machine"):
         W = tf.get_variable("weights", (784, 10),
@@ -39,37 +41,47 @@ def run():#x_test):
                             #initializer=tf.random_normal_initializer())
                             initializer=tf.constant_initializer(0.0))
         y_pred = tf.matmul(X, W) + b
-        #right = tf.reduce_sum(tf.maximum(0., 1 - tf.multiply(y, y_pred)))
-        #right2 = tf.losses.hinge_loss(logits=y_pred, labels=y)
-        #left = tf.reduce_mean(tf.multiply(W, W))
-        #left2 = tf.reduce_mean(tf.square(W))
-        #loss =C*tf.losses.hinge_loss(logits=y_pred, labels=y) + 0.5*tf.reduce_mean(tf.square(W))
-        #loss = tf.reduce_mean(left, axis = 0) * 0.5 + C * tf.reduce_sum(right, axis = 0)
-        #loss = tf.reduce_mean(left) * 0.5 + C * tf.reduce_sum(right) / 508.
+        pred_result = tf.argmax(y_pred, 1)
+        correct_predictions = tf.equal(tf.argmax(y, 1), pred_result)
+        accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
         left = tf.multiply(W, W)
         right = tf.maximum(0., 1 - tf.multiply(y, y_pred))
+        # all these three are ok.
         loss = tf.reduce_mean(left, axis = 0) * 0.5 + C * tf.reduce_sum(right, axis = 0)
         #loss = tf.reduce_mean(left) * 0.5 + C * tf.reduce_sum(right)
+        #loss =C*tf.losses.hinge_loss(logits=y_pred, labels=y)*500. + 0.5*tf.reduce_mean(tf.square(W))
+
 
     operation = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(loss)
-        
+    
     with tf.Session() as sess:
         # Initialize Variables in graph
         sess.run(tf.global_variables_initializer())
-        for i in range(10000):
-            indices = np.random.choice(n_samples, batch_size)
-            X_batch, y_batch = x_train[indices], y_train[indices]
-            #y_batch = np.reshape(y_batch, (batch_size,1))
-            #left_, left2_, right_, right2_ = sess.run([left, left2, right, right2], feed_dict={X: X_batch, y: y_batch})
-            _, loss_ = sess.run([operation, loss], feed_dict={X: X_batch, y: y_batch})
-            if i % 10 == 0:
-                print(loss_)
-                #print(left_)
-                #print(left2_)
-                #print(right_)
-                #print(right2_)
-    
-    predicted_y_test = []
+        
+        predicted_y_test = []
+        
+        for epoch in range(num_epochs):
+            num_batches = n_samples // batch_size
+            for iteration in range(num_batches):
+                indices = np.random.choice(n_samples, batch_size)
+                X_batch, y_batch = x_train[indices], y_train[indices]
+                #x_train_batch, y_train_batch = mnist.train.next_batch(batch_size)
+                _, loss_, acc = sess.run([operation, loss, accuracy], feed_dict={X: X_batch, y: y_batch})
+            print("epoch:",epoch, "loss:", loss_, "acc:",acc)
+            
+        num_test_batches = len(x_test) // batch_size
+        for iteration in range(num_test_batches + 1):
+            if iteration != num_test_batches:
+                x_test_batch = x_test[:batch_size]
+                x_test = x_test[batch_size:]
+            else:
+                x_test_batch = x_test[:len(x_test)]
+                x_test = x_test[len(x_test):]
+            result = sess.run([pred_result], feed_dict={X: x_test_batch})[0]
+            # pay attention to [0] and tolist() here
+            predicted_y_test += result.tolist()
+            
+        print(predicted_y_test)
     
     return predicted_y_test
 
