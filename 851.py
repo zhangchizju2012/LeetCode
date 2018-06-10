312121ba1fa6d5085b565ac9e116d863f75257e5
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2018-06-09 19:52:39 -0700

    may can take a look

--- 851.py
+++ 851.py
@@ -14,42 +14,4 @@ class Solution(object):
         :type quiet: List[int]
         :rtype: List[int]
         """
-        self.quiet = quiet
-        self.dic = {}
-        for i in range(len(quiet)):
-            self.dic[i] = []
-        for item in richer:
-            self.dic[item[1]].append(item[0])
-            
-        result = [0] * len(quiet)
-        self.temp = {}
         
-        for i in range(len(quiet)):
-            if len(self.dic[i]) == 0:
-                self.temp[i] = (i, quiet[i])
-                result[i] = i
-        for i in range(len(quiet)):
-            if len(self.dic[i]) > 0:
-                index, value = self.helper(i)
-                result[i] = index
-        return result
-                        
-    def helper(self, i):
-        if i in self.temp:
-            return self.temp[i]
-        else:
-            index = i
-            value = self.quiet[i]
-            for item in self.dic[i]:
-                if item in self.temp:
-                    index_temp, value_temp = self.temp[item]
-                else:
-                    index_temp, value_temp = self.helper(item)
-                if value_temp < value:
-                    index = index_temp
-                    value = value_temp
-            self.temp[i] = (index, value)
-            return self.temp[i]
-        
-s = Solution()
-print s.loudAndRich([[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]], [3,2,5,4,6,1,7,0])
